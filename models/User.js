504d42763a54ffaf30e2419c7e2824b792d20b8a
odedinaemmanuel84-dev <odedinaemmanuel84@gmail.com>
2025-09-15 23:53:35 +0100

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,10 +1,11 @@
-// models/User.js
 import mongoose from "mongoose";
 
-const UserSchema = new mongoose.Schema({
-  email: { type: String, required: true, unique: true },
-  password: { type: String, required: true },
-});
+const UserSchema = new mongoose.Schema(
+  {
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
-const User = mongoose.model("User", UserSchema);
-export default User;
+export default mongoose.model("User", UserSchema);
